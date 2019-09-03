19754f4d8a26d943f857178c80970341a4228172
Oleg Pisklov <pisklovo@gmail.com>
2019-09-04 00:20:09 +0300

    add comment

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'http://localhost:3000/',
+    baseURL: 'http://localhost:3000/', // should be set based on env
 });
 
 const getUsers = () => instance.get('/users');
