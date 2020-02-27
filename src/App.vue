6c0255010db0dca796f821b6b25c92ecc49ed0a7
Oleg Pisklov <pisklovo@gmail.com>
2020-02-27 22:31:32 +0200

    fixed hydration

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="main">
-        <h1>{{title}}</h1>
-        <router-view></router-view>
+    <div id="app">
+        <div class="main">
+            <h1>{{title}}</h1>
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
